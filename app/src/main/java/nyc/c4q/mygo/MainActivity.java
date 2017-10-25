5226494b8d66b2155c814e216024207cbd05b3cc
Taishawn Williams <twill4317@withs.org>
2017-10-24 22:44:30 -0400

    logic

--- app/src/main/java/nyc/c4q/mygo/MainActivity.java
+++ app/src/main/java/nyc/c4q/mygo/MainActivity.java
@@ -1,13 +1,24 @@
 package nyc.c4q.mygo;
 
+import nyc.c4q.mygo.logic.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView numberField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numberField =(TextView) findViewById(R.id.show_number);
+    }
+
+    public void grabText(View view){
+        numberField.setText(((Button) view).getText().toString());
     }
 }
---
+++ app/src/main/java/nyc/c4q/mygo/logic/GeneralLogic.java
@@ -0,0 +1,15 @@
+package nyc.c4q.mygo.logic;
+
+import android.view.*;
+import android.widget.*;
+
+/**
+ * Created by MarckemX on 10/24/17.
+ */
+
+public class GeneralLogic {
+
+//    public static String grabText(View view){
+//        return ((Button) view).getText().toString();
+//    }
+}
